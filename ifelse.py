9a18dec8dd95b30c72cefbbdffcc33685bd7dcc4
root <sreeharimv555@gmail.com>
2018-07-04 13:31:11 +0000

    new

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+
+#This program is for checkc the number
+a = int(input("Enter the number:"))
+if a < 0:
+    print(" Number is negative")
+elif a > 0:
+    print("Number is positive")
+else:
+    print("Number is zero")
